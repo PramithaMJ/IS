4d0944632e299ef42576d2b9ae5c57d7bc3adb52
Pramitha Jayasooriya <lpramithamj@gmail.com>
2025-06-17 15:43:16 +0530

    Add Crypto and Prime classes for encryption and number theory operations

---
+++ Crypto.java
@@ -0,0 +1,59 @@
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Base64;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
+import javax.crypto.spec.IvParameterSpec;
+
+public class Crypto {
+    public static void main(String[] args)
+            throws InvalidKeyException, InvalidKeySpecException, NoSuchAlgorithmException, NoSuchPaddingException,
+            IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException {
+        String key = "12345678";
+        DESKeySpec desKeySpec = new DESKeySpec(key.getBytes());
+        SecretKey secretKey = SecretKeyFactory.getInstance("DES").generateSecret(desKeySpec);
+        // KeyGenerator keyGen = KeyGenerator.getInstance("DES");
+        // SecretKey secretKey = keyGen.generateKey();
+        // System.out.println("Generated Secret Key: " + secretKey);
+
+        byte[] keyBytes = secretKey.getEncoded();
+        String keyString = new String(keyBytes);
+        System.out.println("Key as String: " + keyString);
+        System.out.println("Secret key Size: " + keyString.length());
+        System.out.println("Secret key Algorithm: " + secretKey.getAlgorithm());
+
+        Cipher des = Cipher.getInstance("DES/CBC/PKCS5Padding"); // ECB mode have vulnerabilities, patterns can be detected in the encrypted data.
+        // We use padding to ensure that the plaintext is a multiple of the block size
+        // and to handle any remaining bytes that do not fit into a complete block.
+
+        // Initializing a vector (IV) for DES encryption.
+        String ivString = "AAAAAAAA"; // IV must be 8 bytes for DES
+        IvParameterSpec iv = new IvParameterSpec(ivString.getBytes());
+
+        des.init(Cipher.ENCRYPT_MODE, secretKey, iv); // Initializing the cipher
+
+        String msg = "I love pizza. You love pizza. Everyone loves pizza.";
+        byte[] msgBytes = msg.getBytes();
+        System.out.println("Plain Text: " + msg);
+
+        // Encrypting the message
+        byte[] encryptedBytes = des.doFinal(msgBytes); // doFinal() method encrypts the data and returns the encrypted bytes.
+        // Display encrypted text as Base64 instead of raw bytes
+        System.out.println("Encrypted Text: " + Base64.getEncoder().encodeToString(encryptedBytes));
+
+        // Decrypting the message - MUST provide the SAME IV
+        des.init(Cipher.DECRYPT_MODE, secretKey, iv); // Added the IV parameter here
+        byte[] decryptedBytes = des.doFinal(encryptedBytes);
+        String decryptedMsg = new String(decryptedBytes);
+        System.out.println("Decrypted Text: " + decryptedMsg);
+    }
+}
---
+++ Prime.java
@@ -0,0 +1,95 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Prime {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        
+        // System.out.print("Enter a number to check if it's prime: ");
+        int number;
+        try {
+            System.out.print("Enter a number: ");
+            number = Integer.parseInt(scanner.nextLine());
+            
+            // if (isPrime(number)) {
+            //     System.out.println(number + " is a prime number");
+            // } else {
+            //     System.out.println(number + " is not a prime number");
+            // }
+            
+            // System.out.print("Do you want to check if two numbers are co-prime? (yes/no): ");
+            // String response = scanner.nextLine();
+            
+            // if (response.equalsIgnoreCase("yes")) {
+            if (true) {
+                System.out.print("Enter a second number: ");
+                try {
+                    int secondNumber = Integer.parseInt(scanner.nextLine());
+                    
+                    BigInteger a = BigInteger.valueOf(number);
+                    BigInteger b = BigInteger.valueOf(secondNumber);
+                    
+                    if (isCoPrime(a, b)) {
+                        System.out.println(number + " and " + secondNumber + " are co-prime");
+                    } else {
+                        System.out.println(number + " and " + secondNumber + " are not co-prime");
+                    }
+
+                    System.out.println("Totient of " + secondNumber + " is: " + totient(b));
+
+                    System.out.println("GCD of " + number + " and " + secondNumber + " is: " + euclidianGCD(number, secondNumber));
+
+
+                } catch (NumberFormatException e) {
+                    System.out.println("Please provide a valid integer for the second number");
+                }
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Please provide a valid integer");
+        } finally {
+            scanner.close();
+        }
+    }
+    
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        if (n <= 3) {
+            return true;
+        }
+        if (n % 2 == 0 || n % 3 == 0) {
+            return false;
+        }
+        
+        for (int i = 5; i * i <= n; i += 6) {
+            if (n % i == 0 || n % (i + 2) == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isCoPrime(BigInteger a, BigInteger b){
+        return a.gcd(b).equals(BigInteger.ONE);
+    }
+
+    public static int totient(BigInteger n){
+        int result = 1;
+        for (int i = 2; i < n.intValue(); i++) {
+            if (isCoPrime(BigInteger.valueOf(i), n)) {
+                result++;
+            }
+        }
+        return result;
+    }
+    
+    public static int euclidianGCD(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
